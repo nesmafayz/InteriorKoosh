8ab79f3843450f4d75d36de6d2d4ff9fcf177f5f
MahmoudRoboost <mahmoud.salah@roboost.app>
2024-11-05 19:26:04 +0200

    make vedio work

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import Typed from 'typed.js';
 
 
@@ -10,7 +10,18 @@ import Typed from 'typed.js';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements AfterViewInit {
+  @ViewChild('heroVideo') heroVideo!: ElementRef<HTMLVideoElement>;
+
   ngAfterViewInit() {
+
+    const video = this.heroVideo.nativeElement;
+    video.muted = true;
+    video.loop = true;
+    video.playsInline = true;
+
+    video.play().catch(error => {
+      console.error("Autoplay failed:", error);
+    });
     const options = {
       strings: ['<span style="color: #cd9c20;">LOVE</span> WHERE YOU LIVE'],
       typeSpeed: 120,
@@ -22,3 +33,4 @@ export class HomeComponent implements AfterViewInit {
   }
 
 }
+
