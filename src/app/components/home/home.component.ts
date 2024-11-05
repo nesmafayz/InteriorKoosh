9e1a6a454fde27df0431412596f16e18d9f60511
nesmafayz <nesmafayez21@gmail.com>
2024-11-05 02:41:25 +0200

    1st commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import Typed from 'typed.js';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const options = {
+      strings: ['<span style="color: #cd9c20;">LOVE</span> WHERE YOU LIVE'],
+      typeSpeed: 120,
+      backSpeed: 25,
+      loop: true,
+      showCursor: false
+    };
+    new Typed('.typed-text', options);
+  }
 
 }
